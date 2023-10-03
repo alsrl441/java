5cbb9b03e3623e5b88959dedb5e4a5f48679e2c3
mingi <128763318+alsrl441@users.noreply.github.com>
2023-10-04 01:33:01 +0900

    Create 실습문제 2.java

---
+++ week02/report/실습문제 2.java
@@ -0,0 +1,27 @@
+package practice;
+
+import java.util.Scanner;
+
+public class IntegerDiscrimination {
+	public static void main(String[] args) {
+		int i;
+		
+		Scanner scanner = new Scanner (System.in);
+		
+		System.out.print("2자리 정수 입력(10~99>>");
+		i = scanner.nextInt();
+		
+		if (i >= 10 && i <= 99) {
+			if (i / 10 == i % 10) {
+				System.out.println("Yes! 10의 자리와 1의 자리가 같습니다.");
+			} else {
+				System.out.println("No! 10의 자리와 1의 자리가 같지 않습니다.");
+			}
+		} else {
+			System.out.println("10 ~ 99 사이의 정수를 입력해주세요.");
+		}
+		
+		
+	}
+
+}
