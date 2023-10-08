ec20d989de7b205c7ca87f576b3c4684a52b7ee8
mingi <128763318+alsrl441@users.noreply.github.com>
2023-10-08 17:00:58 +0900

    Create 실습문제 12(1).java

---
+++ week03/report/실습문제 12(1).java
@@ -0,0 +1,34 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Practice2_12_1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+		double i, j;
+		String operator;
+		
+		System.out.print("연산>>");
+		i = scanner.nextDouble();
+		operator = scanner.next();
+		j = scanner.nextDouble();
+		
+		
+		if (operator == "/" && j == 0 || j == 0.0) {
+			System.out.println("0으로 나눌 수 없습니다.");
+		}else if (operator.equals("+")) {
+			System.out.println(i + " + " + j + " = " + (i + j));
+		} else if (operator.equals("-")) {
+			System.out.println(i + " - " + j + " = " + (i - j));
+		} else if (operator.equals("*")) {
+			System.out.println(i + " * " + j + " = " + (i * j));
+		} else if (operator.equals("/")) {
+			System.out.println(i + " / " + j + " = " + (i / j));
+		} else {
+			System.out.println("+, -, *, / 4개의 연산자만 사용할 수 있습니다.");
+		}
+		
+		
+		scanner.close();
+	}
+}
