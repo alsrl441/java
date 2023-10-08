6499002ec6f4b815e8706fa1da097c4823b72ce6
mingi <128763318+alsrl441@users.noreply.github.com>
2023-10-08 16:33:41 +0900

    Create 실습문제 11(1).java

---
+++ week03/report/실습문제 11(1).java
@@ -0,0 +1,28 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Practice2_11 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+		
+		int month;
+		
+		System.out.print("달을 입력하세요(1~12)>>");
+		month = scanner.nextInt();
+		
+		if (month >= 3 && month <= 5) {
+			System.out.println("봄");
+		} else if (month >= 6 && month <= 8) {
+			System.out.println("여름");
+		} else if (month >= 9 && month <= 11) {
+			System.out.println("가을");
+		} else {
+			System.out.println("겨울");
+		}
+		
+		
+		
+		
+	}
+}
