e69af6a5aaa4dbefc02c7f6efbd9e1059334bec2
mingi <128763318+alsrl441@users.noreply.github.com>
2023-10-08 17:21:31 +0900

    Create 실습문제 1(3).java

---
+++ week04/report/실습문제 1(3).java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Practice3_1_3 {
+	public static void main(String[] args) {
+		int sum = 0, i = 0;
+		for (i = 0; i < 100; i += 2) {
+			sum = sum + i;
+		}
+		System.out.println(sum);
+	}
+}
