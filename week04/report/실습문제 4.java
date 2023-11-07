f533ce5e458c889e3d94ea3ee69d72aa56fe2c96
mingi <128763318+alsrl441@users.noreply.github.com>
2023-11-07 23:57:04 +0900

    Update 실습문제 4.java

--- week04/report/실습문제 4.java
+++ week04/report/실습문제 4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
  
-public class Num3 {
+public class AlphabetInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("소문자 알파벳 하나를 입력하시오>>");
