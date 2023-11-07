9c059eb07989fd5d050a75f87bab908b694ce96c
mingi <128763318+alsrl441@users.noreply.github.com>
2023-11-07 23:52:03 +0900

    Update 실습문제 3.java

--- week04/report/실습문제 3.java
+++ week04/report/실습문제 3.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
  
-public class num_3 {
+public class Num3 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("정수를 입력하시오>> ");
-        int num = sc.nextInt();
+        int num = scanner.nextInt();
         
         for(int i=1;i<=num;i++) {
             for(int j=num;j>=i;j--) {
