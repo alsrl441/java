daa57dc77d358aa36bda9e4ceed437324d29941a
mingi <128763318+alsrl441@users.noreply.github.com>
2023-11-07 23:58:14 +0900

    Update 실습문제 3.java

--- week04/report/실습문제 3.java
+++ week04/report/실습문제 3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
  
-public class Num3 {
+public class PrintStar {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("정수를 입력하시오>> ");
