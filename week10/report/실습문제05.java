4f1f0f041aab949d76389ae6ad213c51d32e76e7
mingi <128763318+alsrl441@users.noreply.github.com>
2023-11-15 02:04:40 +0900

    Create 실습문제05.java

---
+++ week10/report/실습문제05.java
@@ -0,0 +1,44 @@
+package prctice;
+
+class Point{
+    private int x,y;
+    public Point (int x, int y) {
+      this.x = x; this.y = y;
+    }
+    public int getX() {
+      return x;
+    }
+    public int getY() {
+      return y;
+    }
+    protected void move(int x, int y) {
+      this.x = x; this.y = y;
+    }
+}
+ 
+class ColorPoint extends Point{
+    private String Color = null;
+    public ColorPoint(int x, int y, String Color){
+        super(x,y);
+        this.Color = Color;
+    }
+    public void setXY(int x, int y) {
+      move(x,y);
+    }
+    public void setColor(String Color) {
+      this.Color = Color;
+    }
+    public String toString() {
+        return Color + "색의 (" + getX() + "," + getY()  +")의 점";
+    }
+}
+ 
+public class num_5 {
+    public static void main(String[] args) {
+        ColorPoint cp = new ColorPoint(5, 5, "YELLOW");
+        cp.setXY(10, 20);
+        cp.setColor("RED");
+        String str = cp.toString();
+        System.out.println(str + "입니다.");
+    }
+}
