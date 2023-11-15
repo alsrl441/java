b2f318cb5e32fa0a428697181f9d0004f634b20a
mingi <128763318+alsrl441@users.noreply.github.com>
2023-11-15 11:10:19 +0900

    Create Chapter6Num1.java

---
+++ week11/report/Chapter6Num1.java
@@ -0,0 +1,29 @@
+package practice;
+
+class MyPoint {
+	int x, y;
+
+	public MyPoint(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "Point(" + x + "," + y + ")";
+	}
+}
+
+public class Capter6Num1 {
+	public static void main(String[] args) {
+		MyPoint p = new MyPoint(3, 50);
+		MyPoint q = new MyPoint(4, 50);
+		System.out.println(p);
+		if(p.equals(q)) {
+			System.out.println("같은 점");
+		} else {
+			System.out.println("다른 점");
+		}
+	}
+}
