db68d7f541c564890ef33434dbb8a5dfd68eaedc
mingi <128763318+alsrl441@users.noreply.github.com>
2023-11-22 11:54:31 +0900

    Create Chapter7Num3.java

---
+++ week12/report/Chapter7Num3.java
@@ -0,0 +1,35 @@
+package practice;
+
+import java.util.*;
+
+public class Chapter7Num3 {
+	public static void main(String[] args) {
+		HashMap<String, Integer> nations = new HashMap<String, Integer>();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("나라의 이름과 인구를 입력하세요.(예: Korea 5000)");
+		while(true) {
+			System.out.print("나라 이름, 인구>> ");
+			String name = sc.next();
+			if(name.equals("그만"))
+				break;
+			int population = sc.nextInt();
+			
+			nations.put(name, population);
+		}
+		
+		while(true) {
+			System.out.print("인구 검색>>");
+			String name = sc.next();
+			if(name.equals("그만")) {
+				System.out.println("종료합니다.");
+				break;
+			}
+			if(nations.get(name) == null)
+				System.out.println(name + "나라는 없습니다.");
+			else
+				System.out.println(name + "의 인구는 " + nations.get(name));
+		}
+		sc.close();
+  }
+}
