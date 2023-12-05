77cbd0ec2b48b0905f594a1ff371426127d71e1a
mingi <128763318+alsrl441@users.noreply.github.com>
2023-12-05 22:04:25 +0900

    Create Chapter09Num01.java

---
+++ week13/report/Chapter09Num01.java
@@ -0,0 +1,17 @@
+package practice;
+
+import javax.swing.JFrame;
+
+public class Chapter09Num01 extends JFrame {
+	Chapter09Num01() {
+		setTitle("Let's study Java");
+		setSize(400, 200);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Chapter09Num01();
+	}
+}
