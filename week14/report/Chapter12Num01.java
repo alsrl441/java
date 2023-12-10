816e4f8f7f747aad0e8634190d79dd684f5a3882
mingi <128763318+alsrl441@users.noreply.github.com>
2023-12-11 01:29:41 +0900

    Create Chapter12Num01.java

---
+++ week14/report/Chapter12Num01.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Chapter12Num01 extends JFrame {
+	private MyPanel panel = new MyPanel();
+
+	public Chapter12Num01() {
+		setTitle("이미지 그리기 연습");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(panel);
+		setSize(300, 300);
+		setVisible(true);
+	}
+
+	class MyPanel extends JPanel {
+		private JButton btn = new JButton("Hide/Show");
+		private ImageIcon icon = new ImageIcon("images/image0.jpg");
+		private Image image = icon.getImage();
+		
+		public MyPanel() {
+			setLayout(new FlowLayout());
+			add(btn);
+		}
+
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), this);
+		}
+	}
+
+	public static void main(String[] args) {
+		new Chapter12Num01();
+	}
+
+}
