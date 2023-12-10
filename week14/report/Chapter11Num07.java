da3cc0e2f796cee088186223f061e157862c063e
mingi <128763318+alsrl441@users.noreply.github.com>
2023-12-11 01:26:38 +0900

    Create Chapter11Num07.java

---
+++ week14/report/Chapter11Num07.java
@@ -0,0 +1,41 @@
+package practice;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+
+public class Chapter11Num07 extends JFrame {
+    Chapter11Num07(){
+        JSlider slider = new JSlider(0, 100, 0); // min. max. init value
+        JLabel label = new JLabel("I Love Java");
+        setTitle("JSlider Practice Frame");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Container c = getContentPane();
+        c.setLayout(new GridLayout(3,1));
+        slider.setMajorTickSpacing(20);
+        slider.setMinorTickSpacing(5);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+
+
+        slider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                JSlider temp = (JSlider) e.getSource();
+                label.setFont(new Font("Arial", Font.PLAIN, temp.getValue()));
+            }
+        });
+
+        c.add(slider, BorderLayout.NORTH);
+        c.add(label, BorderLayout.CENTER);
+
+        setSize(500, 500);
+        setVisible(true);
+    }
+
+
+    public static void main(String args[]){
+        new Chapter11Num07();
+    }
+}
